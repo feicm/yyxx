758a17875aeeee0c6d1e52cd0edcbcfeda21b1d7
feicm <chenlonghui@cyou-inc.com>
2017-05-27 15:18:15 +0800

    修改首页快捷链接

--- src/components/home/Main.vue
+++ src/components/home/Main.vue
@@ -4,68 +4,35 @@
             <router-link :to="'/user/info'">
                 <mt-button size="large" plain>用户中心</mt-button>
             </router-link>
-            <router-link :to="'/user/attestation'">
-                <mt-button size="large" plain>认证信息</mt-button>
-            </router-link>
-            <router-link :to="'/user/classgroup/create'">
-                <mt-button size="large" plain>创建班群</mt-button>
-            </router-link>
             <router-link :to="'/user/classgroup'">
                 <mt-button size="large" plain>我的班群</mt-button>
             </router-link>
             <router-link :to="'/user/classgroup/join'">
                 <mt-button size="large" plain>加入班群</mt-button>
             </router-link>
-            <router-link :to="'/user/classgroup/notice/1'">
-                <mt-button size="large" plain>班群公告</mt-button>
-            </router-link>
-            <router-link :to="'/user/recharge'">
-                <mt-button size="large" plain>充值</mt-button>
-            </router-link>
-            <router-link :to="'/user/classgroup/task/1'">
-                <mt-button size="large" plain>任务</mt-button>
-            </router-link>
-            <router-link :to="'/user/classgroup/task/history/1'">
-                <mt-button size="large" plain>历史任务</mt-button>
-            </router-link>
-            <router-link :to="'/user/feedback'">
-                <mt-button size="large" plain>意见反馈</mt-button>
+            <a href="/user/recharge/?#/user/recharge">
+                <mt-button size="large" plain>快速充值</mt-button>
+            </a>
+            <router-link :to="'/default'">
+                <mt-button size="large" plain>字母音标</mt-button>
             </router-link>
             <router-link :to="'/default'">
-                <mt-button size="large" plain>空页面</mt-button>
+                <mt-button size="large" plain>单词背诵</mt-button>
+            </router-link>
+            <router-link :to="'/default'">
+                <mt-button size="large" plain>同步教学</mt-button>
+            </router-link>
+            <router-link :to="'/default'">
+                <mt-button size="large" plain>考题演练</mt-button>
+            </router-link>
+            <router-link :to="'/default'">
+                <mt-button size="large" plain>讲台</mt-button>
             </router-link>
         </div>
     </div>
 </template>
 
 <script>
-  import Vue from 'vue'
-  import {mapGetters} from 'vuex'
-  import {Button} from 'mint-ui';
-  import * as types from '../../store/types'
-  import axios from 'axios'
-  import Store from 'store'
-
-
-  Vue.component(Button.name, Button);
-
-  export default {
-    beforeMount(){
-
-    },
-    data () {
-      return {}
-    },
-    watch: {
-    },
-    computed: mapGetters({
-      wxAuthInfo: 'wxAuthInfo'
-    }),
-    components: {
-    },
-    methods: {
-    }
-  }
 
 </script>
 
