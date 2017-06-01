f232138da277b38b6f0495eb628ac6ac67ae9a2f
feicm <chenlonghui@cyou-inc.com>
2017-06-01 09:17:35 +0800

    支付回调异常判断

--- src/components/recharge/Main.vue
+++ src/components/recharge/Main.vue
@@ -105,6 +105,9 @@
                         that.$router.replace('/user/info')
                       }, 1200)
                     }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                    else{
+                      alert(res.err_msg)
+                    }
                   }
                 );
               }
